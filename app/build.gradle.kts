2ab4759eac23a953e11e8fd4785597e6d0810f0e
mapapicv@gmail.com <mapapicv@gmail.com>
2025-10-15 22:06:04 -0300

    Login a página principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.navigation:navigation-compose:2.7.2") // or latest version
+
     // Dependencias locales
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
--- app/src/main/java/com/example/appecoviaje/ui/LoginScreen.kt
+++ app/src/main/java/com/example/appecoviaje/ui/LoginScreen.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.appecoviaje.theme.AppEcoViajeTheme
+import com.example.appecoviaje.ui.theme.AppEcoViajeTheme
 import com.example.appecoviaje.viewmodel.LoginViewModel
 import com.example.appecoviaje.viewmodel.LoginUiState
 
